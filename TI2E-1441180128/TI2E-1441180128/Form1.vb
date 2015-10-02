d8b12f96147014eb1c25ef83f03cd05c33f39ec1
Alifias <alifiaesh@gmail.com>
2015-10-02 17:30:11 +0700

    Tugas 1

---
+++ TI2E-1441180128/TI2E-1441180128/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Timer1.Enabled = True Then
+            Button1.Text = "START"
+            Timer1.Enabled = False
+        Else
+            Timer1.Enabled = True
+            Button1.Text = "STOP"
+            Me.BackColor = Color.Maroon
+        End If
+    End Sub
+
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If Me.BackColor = Color.Maroon Then
+            Me.BackColor = Color.Black
+        Else
+            Me.BackColor = Color.Maroon
+        End If
+    End Sub
+End Class
